f7e5b3184cebe079dfe12c6d65530e4fc503c44c
mirodietiker <mirodietiker@227761.no-reply.drupal.org>
2017-12-21 15:11:15 +0100

    Issue #2923301 by toncic, pivica, miro_dietiker: Widget actions hook doesn't have information about translating status; API doc fix

--- paragraphs.api.php
+++ paragraphs.api.php
@@ -42,7 +42,7 @@ function hook_paragraphs_behavior_info_alter(&$paragraphs_behavior) {
  *   - paragraphs_entity: the paragraphs entity for this widget. Might be
  *     unsaved, if we have just added a new item to the widget.
  *   - is_translating: Boolean if the widget is translating.
- *   - allow_structure_changes: Boolean if changes to structure are OK.
+ *   - allow_reference_changes: Boolean if changes to structure are OK.
  */
 function hook_paragraphs_widget_actions_alter(array &$widget_actions, array &$context) {
 }
