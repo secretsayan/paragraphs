45a4dbf3622447c03e91b8553c97a7b314bc6e02
Mathilde Dumond <55979-mathilde_dumond@users.noreply.drupalcode.org>
2022-08-25 17:10:17 +0200

    Issue #3199551 by romina_ferrario, JeroenT, mathilde_dumond, nuez, mrinalini9: The content/behavior tabs on the paragraphs widget don't render properly in Claro

--- src/Plugin/Field/FieldWidget/ParagraphsWidget.php
+++ src/Plugin/Field/FieldWidget/ParagraphsWidget.php
@@ -1061,7 +1061,7 @@ class ParagraphsWidget extends WidgetBase {
     $field_prefix = strtr($this->fieldIdPrefix, '_', '-');
     if (count($this->fieldParents) == 0) {
       if ($items->getEntity()->getEntityTypeId() != 'paragraph') {
-        $tabs = '<ul class="paragraphs-tabs tabs primary clearfix"><li class="tabs__tab paragraphs_content_tab"><a href="#' . $field_prefix . '-values">' . $this->t('Content', [], ['context' => 'paragraphs']) . '</a></li><li class="tabs__tab paragraphs_behavior_tab"><a href="#' . $field_prefix . '-values">' . $this->t('Behavior', [], ['context' => 'paragraphs']) . '</a></li></ul>';
+        $tabs = '<ul class="paragraphs-tabs tabs primary tabs--secondary paragraphs-tabs-hide clearfix"><li class="tabs__tab paragraphs_content_tab"><a href="#' . $field_prefix . '-values" class="tabs__link">' . $this->t('Content', [], ['context' => 'paragraphs']) . '</a></li><li class="tabs__tab paragraphs_behavior_tab"><a href="#' . $field_prefix . '-values" class="tabs__link">' . $this->t('Behavior', [], ['context' => 'paragraphs']) . '</a></li></ul>';
       }
     }
     if (count($this->fieldParents) > 0) {
